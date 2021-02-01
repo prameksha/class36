47e5b49bcb1c31b50b8316c5cc6e3421600b15e5
prameksha <71688843+prameksha@users.noreply.github.com>
2021-02-01 12:57:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var canvas,backgroundImage,playerCount,database;
+var gameState=0;
+var form,player,game; 
+
+
+function setup(){
+
+ canvas= createCanvas(400,400);
+ database = firebase.database();
+
+ game =new Game();
+ game.getState();
+ game.start();
+
+}
+
+function draw(){
+  background("white");
+  
+  
+    
+  
+}
+
+
